132dd08ba7d97db2619ab187bbe795b8bd54dad5
Tomislav Ficko <ficko.tomislav@gmail.com>
2020-12-26 22:28:49 +0100

    Add files for activities which will be used

---
+++ app/src/main/java/hr/ficko/transactionmonitor/ui/LoginActivity.kt
@@ -0,0 +1,6 @@
+package hr.ficko.transactionmonitor.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+}
--- app/src/main/java/hr/ficko/transactionmonitor/ui/MainActivity.kt
+++ app/src/main/java/hr/ficko/transactionmonitor/ui/MainActivity.kt
@@ -2,9 +2,16 @@ package hr.ficko.transactionmonitor.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import hr.ficko.transactionmonitor.R
+import hr.ficko.transactionmonitor.databinding.ActivityMainBinding
+import hr.ficko.transactionmonitor.viewModels.RepositoryViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<RepositoryViewModel>()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
