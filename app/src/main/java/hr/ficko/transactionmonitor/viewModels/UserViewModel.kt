d5fa3b4ac8f3004e39a77998507aaa5e6ddae227
Tomislav Ficko <ficko.tomislav@gmail.com>
2020-12-30 14:47:22 +0100

    Fix error

--- app/src/main/java/hr/ficko/transactionmonitor/viewModels/UserViewModel.kt
+++ app/src/main/java/hr/ficko/transactionmonitor/viewModels/UserViewModel.kt
@@ -73,7 +73,7 @@ class UserViewModel @ViewModelInject constructor(
                     )
                 )
             }
-            VALID -> {
+            NameValidationStatus.VALID -> {
                 persistNames(name, surname)
                 nameValidationLiveData.postValue(
                     NameError(
